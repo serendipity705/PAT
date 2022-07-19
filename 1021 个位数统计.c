0e877365dc0e25fdbd2e09cb5186ad20e19b55e8
纸君 <101634494+serendipity705@users.noreply.github.com>
2022-07-19 17:41:45 +0800

    1021 个位数统计
    给定一个 k 位整数 N=d
    k−1
    ​
     10
    k−1
     +⋯+d
    1
    ​
     10
    1
     +d
    0
    ​
      (0≤d
    i
    ​
     ≤9, i=0,⋯,k−1, d
    k−1
    ​
     >0)，请编写程序统计每种不同的个位数字出现的次数。例如：给定 N=100311，则有 2 个 0，3 个 1，和 1 个 3。
    
    输入格式：
    每个输入包含 1 个测试用例，即一个不超过 1000 位的正整数 N。
    
    输出格式：
    对 N 中每一种不同的个位数字，以 D:M 的格式在一行中输出该位数字 D 及其在 N 中出现的次数 M。要求按 D 的升序输出。

--- 1021 个位数统计.c
+++ 1021 个位数统计.c
@@ -3,7 +3,7 @@ int main(int argc, char *argv[]) {
 	int s[11] = {0};
 	char N[1001];
 	scanf("%s", &N);
-	for ( int i=0; N[i]!=0; i++ )	s[N[i]-'0']++;
+	for ( int i=0; N[i]!='\0'; i++ )	s[N[i]-'0']++;
 	for ( int i=0; i<10; i++ )	if( s[i] > 0 ) printf("%d:%d\n", i, s[i]);	
 	return 0;
 }
